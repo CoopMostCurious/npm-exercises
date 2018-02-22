7e0cdff957b0a69fb19406aade4a9ded3a7a366f
Cooper Hanke <hanke31314@gmail.com>
2018-02-22 11:33:55 -0600

    exercise is completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+const $ = require('jquery');
+
 const sayHello = () => console.log(`hello`);
 sayHello();
+
+$(document).ready( () => {
+    $(`h1`).replaceWith(`<h3>Welcome to this webpage!</h3><p>There's nothing special other than the fact you are here.</p>`);
+    $(`p`).append(`<p>If you see me, then JQuery is indeed operational on this site.</p>`);
+});
