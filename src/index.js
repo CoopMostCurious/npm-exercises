8d75772739d2af80a1df15e609a53f85c5af462b
Cooper <cooper@localhost.localdomain>
2018-02-22 15:46:22 -0600

    'first part of bonus done'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const $ = require('jquery');
 const sayHello = () => console.log(`hello`);
 sayHello();
 
-$(document).ready( () => {
-    $(`h1`).replaceWith(`<h3>Welcome to this webpage!</h3><p>There's nothing special other than the fact you are here.</p>`);
-    $(`p`).first().append(`<p>If you see me, then JQuery is indeed operational on this site.</p>`);
+$(document).ready(() => {
+  $(`h1`).replaceWith(`<h3>Welcome to this webpage!</h3><p>There's nothing special other than the fact you are here.</p>`)
+  $(`p`).first().append(`<p>If you see me, then JQuery is indeed operational on this site.</p>`)
 });
