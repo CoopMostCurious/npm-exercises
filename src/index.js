dcb36b07631ba1c39895387296cd06208e09654c
Cooper Hanke <hanke31314@gmail.com>
2018-02-22 20:02:16 -0600

    finished the modules exercise and using exercise to troubleshoot RHEL 7 VM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const $ = require('jquery');
+const hello = require('./say-hello.js')
+
 
 const sayHello = () => console.log(`hello`);
 sayHello();
+hello();
 
 $(document).ready(() => {
   $(`h1`).replaceWith(`<h3>Welcome to this webpage!</h3><p>There's nothing special other than the fact you are here.</p>`)
--- src/say-hello.js
+++ src/say-hello.js
@@ -1,3 +1,3 @@
 const hello = () => console.log(`Hello there!`);
 
-module.exports = hello();
+module.exports = hello;
